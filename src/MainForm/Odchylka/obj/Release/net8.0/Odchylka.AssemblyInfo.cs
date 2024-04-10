351f34525c58a59acc11d8b7b6d83e15496cb940
Jan Hrdina <jan.hrdina99@gmail.com>
2024-04-10 16:36:16 +0200

    cached unstaged files idk bro pls help me im under the water

--- src/MainForm/Odchylka/obj/Release/net8.0/Odchylka.AssemblyInfo.cs
+++ src/MainForm/Odchylka/obj/Release/net8.0/Odchylka.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odchylka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154098b112083b41a4f07c3e284eb720cb385bd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce3366ca246e432811c795b52e5b229593de04f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odchylka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odchylka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
